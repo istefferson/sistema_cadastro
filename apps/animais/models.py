8517484d711ff9e0e47760175b42318d58a4acd3
Istefferson <isteffersonsilva@gmail.com>
2020-09-27 09:33:15 -0300

    27/09/2020 06:56

--- apps/animais/models.py
+++ apps/animais/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Animais(models.Model):
+    animal_nome = models.TextField()
+    animal_raca = models.TextField()
+    animal_peso = models.FloatField()
+
+    def __str__(self):
+        return self.animal_nome
+
 # Create your models here.
